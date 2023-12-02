461aa84249a5cb6698ec1ac9cc32a7c42e2938b9
Stephane Philippart <wildagsx@gmail.com>
2023-12-02 16:39:11 +0100

    feat: 🎉 Day 2

--- src/main/java/com/ovhcloud/java/util/PuzzleInputs.java
+++ src/main/java/com/ovhcloud/java/util/PuzzleInputs.java
@@ -2,7 +2,10 @@ package com.ovhcloud.java.util;
 
 public enum PuzzleInputs {
   INPUT1_1("/input-day1-1.txt"),
-  INPUT1_2("/input-day1-2.txt");
+  INPUT1_2("/input-day1-2.txt"),
+  INPUT2_1("/input-day2-1.txt"),
+  INPUT2_2("/input-day2-2.txt"),
+  INPUT2_TEST("/input-day2-Test.txt");
 
   public final String label;
 
